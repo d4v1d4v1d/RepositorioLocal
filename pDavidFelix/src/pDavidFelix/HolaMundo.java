aa5d208cbb07ba65c58d40f15c281388992524f8
Arthur <Arthur@192.168.1.35>
2022-04-26 17:29:52 +0200

    Cuarto Cambio

---
+++ pDavidFelix/src/pDavidFelix/HolaMundo.java
@@ -0,0 +1,19 @@
+package pDavidFelix;
+
+public class HolaMundo {
+	
+	public static void main (String []args) {
+		
+		System.out.println("Hola Mundo");
+		
+		System.out.println("Hola Gente");
+		
+		System.out.println("Adios Mundo");
+		
+		System.out.println("Adios Gente");
+		
+		System.out.println("Hola");
+	}
+	
+
+}
