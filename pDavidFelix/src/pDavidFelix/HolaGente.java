a52a869d080a51b67d846bc39dcdaf63943c79f0
Arthur <Arthur@192.168.1.35>
2022-04-26 18:02:05 +0200

    Octavo Cambio

---
+++ pDavidFelix/src/pDavidFelix/HolaGente.java
@@ -0,0 +1,19 @@
+
+public class HolaGente {
+
+	public static void main (Striing [] args) {
+		
+		void contador() {
+			
+			int x;
+			
+			while(x<=5) {
+				x=x+1;
+				
+			}
+			
+		}
+		
+	}
+	
+}
